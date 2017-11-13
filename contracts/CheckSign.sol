7709bdecf480624dd6f5d0124188742de28f3193
Edouard FISCHER <baleinesurseine@users.noreply.github.com>
2017-11-13 09:28:25 +0100

    hardcoded values

--- contracts/CheckSign.sol
+++ contracts/CheckSign.sol
@@ -24,13 +24,14 @@ contract CheckSign {
 
   function hashTyped(uint value) internal pure returns (bytes32) {
     var h1 = keccak256("uint message");
+    // var h1 = hex"0xa0cad2a27d1258dabaed1f22e79f9d5873088cd468085ac27127d7185f9925b1";
     var h2 = keccak256(value);
     return keccak256(h1, h2);
   }
 
   function hashTypedDb(uint value, string mess) internal pure returns (bytes32) {
     var h1 = keccak256("string Message", "uint Amount");
-    // h1 could be hardcoded "0x783f25e807da0f13fe76dd23e34676bb66bc34b6fb165dc3c2850ee6bca74fa4"
+    // var h1 = hex"0x783f25e807da0f13fe76dd23e34676bb66bc34b6fb165dc3c2850ee6bca74fa4";
     var h2 = keccak256(mess, value);
     return keccak256(h1, h2);
   }
